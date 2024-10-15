a93c58f3f5cf2ee8b7ae5c88635b0b58e74c5016
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-10-15 11:24:06 +0300

    Added model for page with notifications

---
+++ TaskMaster/TaskMaster.Core/Models/Notification/NotificationPageModel.cs
@@ -0,0 +1,23 @@
+﻿namespace TaskMaster.Core.Models.Notification
+{
+    /// <summary>
+    /// Model for page with Notifications
+    /// </summary>
+    public class NotificationPageModel
+    {
+        /// <summary>
+        /// Collection of Notifications for page
+        /// </summary>
+        public IEnumerable<NotificationInfoModel> Notifications { get; set; } = new List<NotificationInfoModel>();
+
+        /// <summary>
+        /// Pages count
+        /// </summary>
+        public double PagesCount { get; set; }
+
+        /// <summary>
+        /// Current page
+        /// </summary>
+        public int CurrentPage { get; set; }
+    }
+}
