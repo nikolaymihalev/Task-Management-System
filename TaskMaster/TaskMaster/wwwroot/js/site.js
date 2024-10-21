f9aaf8606ed6606dca3051c16e85bfecf71b51e8
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-10-21 10:51:39 +0300

    Added function to change sidebar active buttons

--- TaskMaster/TaskMaster/wwwroot/js/site.js
+++ TaskMaster/TaskMaster/wwwroot/js/site.js
@@ -1,4 +1,9 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function changeSidebarActiveButton(index) {
+    const sidebarLinkEls = document.querySelectorAll('#sidebarMenu a');
 
-// Write your JavaScript code.
+    for (el of sidebarLinkEls) {
+        el.classList.remove('active');
+    }
+
+    sidebarLinkEls[index].classList.add('active');
+}
