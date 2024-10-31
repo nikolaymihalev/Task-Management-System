c439aa2357db71c70d945c2dfd5a0b17e0b6c2b3
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-10-31 11:30:09 +0300

    Created NotificationService tests file

---
+++ TaskMaster/TaskMaster.UnitTests/NotificationServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using TaskMaster.Infrastructure.Models;
+
+namespace TaskMaster.UnitTests
+{
+    [TestFixture]
+    public class NotificationServiceTests
+    {
+        private ApplicationDbContext context;
+        private IRepository repository;
+        private INotificationService notificationService;
+        private Notification not;
+        private Notification not2;
+        private static string userId = Guid.NewGuid().ToString();
+
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            this.context.Database.EnsureDeleted();
+            this.context.Dispose();
+        }
+    }
+}
