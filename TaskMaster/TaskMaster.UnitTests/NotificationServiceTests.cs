96eb58c920efd0f5378b7142d4930a6b22454f5d
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-10-31 11:33:20 +0300

    Initialize properties for tests

--- TaskMaster/TaskMaster.UnitTests/NotificationServiceTests.cs
+++ TaskMaster/TaskMaster.UnitTests/NotificationServiceTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Threading.Tasks;
-using TaskMaster.Infrastructure.Models;
+﻿using TaskMaster.Infrastructure.Models;
 
 namespace TaskMaster.UnitTests
 {
@@ -16,7 +15,36 @@ namespace TaskMaster.UnitTests
         [SetUp]
         public void SetUp()
         {
-            
+            not = new Notification()
+            {
+                Id = 1,
+                Message = "Test Message",
+                DateSent = new DateTime(2024, 10, 11),
+                UserId = userId,
+            };
+
+            not2 = new Notification()
+            {
+                Id = 2,
+                Message = "Test Message 2",
+                DateSent = new DateTime(2024, 11, 12),
+                UserId = userId,
+            };
+
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+               .UseInMemoryDatabase(databaseName: "TaskMasterDb")
+               .Options;
+
+            this.context = new ApplicationDbContext(options);
+
+            this.repository = new Repository(this.context);
+
+            this.repository.AddAsync(not);
+            this.repository.AddAsync(not2);
+
+            this.repository.SaveChangesAsync();
+
+            notificationService = new NotificationService(this.repository);
         }
 
         [TearDown]
