85dfa2c8091e72c2e4370a7b846a0f8c0e39f08b
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-11-01 12:14:22 +0300

    Created StatisticsService tests file

---
+++ TaskMaster/TaskMaster.UnitTests/StatisticsServiceTests.cs
@@ -0,0 +1,28 @@
+﻿namespace TaskMaster.UnitTests
+{
+    [TestFixture]
+    public class StatisticsServiceTests
+    {
+        private ApplicationDbContext context;
+        private IRepository repository;
+        private static string userId = Guid.NewGuid().ToString();
+        private IStatisticsService statisticsService;
+        private Infrastructure.Models.Task task;
+        private Infrastructure.Models.Task task2;
+        private Infrastructure.Models.Task task3;
+        private Infrastructure.Models.Task task4;
+
+        [SetUp]
+        public void SetUp() 
+        {
+
+        }
+
+        [TearDown]
+        public void TearDown() 
+        {
+            this.context.Database.EnsureDeleted();
+            this.context.Dispose();
+        }
+    }
+}
