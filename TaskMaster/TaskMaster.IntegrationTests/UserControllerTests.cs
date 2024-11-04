e53147444df4072215741ab0391b22df848858ac
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-11-04 12:28:08 +0300

    Created UserController Tests file

---
+++ TaskMaster/TaskMaster.IntegrationTests/UserControllerTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Moq;
+using TaskMaster.Controllers;
+
+namespace TaskMaster.IntegrationTests
+{
+    public class UserControllerTests
+    {
+        private readonly Mock<UserManager<IdentityUser>> _userManagerMock;
+        private readonly Mock<SignInManager<IdentityUser>> _signInManagerMock;
+        private readonly UserController _userController;
+    }
+}
