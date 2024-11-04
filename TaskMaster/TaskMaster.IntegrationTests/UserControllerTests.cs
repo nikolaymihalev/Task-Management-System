0e469ad647415e98d71f712ff98467c9c218ec56
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-11-04 12:53:08 +0300

    Added test for MyTasks view

--- TaskMaster/TaskMaster.IntegrationTests/UserControllerTests.cs
+++ TaskMaster/TaskMaster.IntegrationTests/UserControllerTests.cs
@@ -5,6 +5,7 @@ using Moq;
 using System.Security.Claims;
 using TaskMaster.Controllers;
 using TaskMaster.Core.Contracts;
+using TaskMaster.Core.Models.Task;
 using TaskMaster.Core.Models.User;
 using Xunit;
 using Assert = Xunit.Assert;
@@ -19,7 +20,7 @@ namespace TaskMaster.IntegrationTests
         private readonly Mock<IStatisticsService> _statisticsServiceMock;
         private readonly Mock<ITaskService> _taskServiceMock;
         private readonly Mock<INotificationService> _notificationServiceMock;
-        private readonly UserController _controller;
+        private readonly UserController _userController;
         private static readonly string userId = "bd68a836-f988-4dea-af8d-ad33664480af";
 
         public UserControllerTests()
@@ -37,7 +38,7 @@ namespace TaskMaster.IntegrationTests
             _taskServiceMock = new Mock<ITaskService>();
             _notificationServiceMock = new Mock<INotificationService>();
 
-            _controller = new UserController(
+            _userController = new UserController(
                 _userManagerMock.Object,
                 _signInManagerMock.Object,
                 _statisticsServiceMock.Object,
@@ -55,22 +56,47 @@ namespace TaskMaster.IntegrationTests
                 .Setup(service => service.GetStatisticsAsync(It.IsAny<string>()))
                 .ReturnsAsync(expectedModel);
 
-            var userClaimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
-            {
-                new Claim(ClaimTypes.NameIdentifier, userId)
-            }));
+            _userController.ControllerContext = CreateController();
 
-            _controller.ControllerContext = new ControllerContext
-            {
-                HttpContext = new DefaultHttpContext { User = userClaimsPrincipal }
-            };
-
-            var result = await _controller.Dashboard() as ViewResult;
+            var result = await _userController.Dashboard() as ViewResult;
 
             Assert.NotNull(result);
             Assert.IsType<ViewResult>(result);
             Assert.Equal(expectedModel, result.Model);
             _statisticsServiceMock.Verify(service => service.GetStatisticsAsync(userId), Times.Once);
+        }
+
+        [Fact]
+        public async Task Test_MyTasksReturnsViewWithModel()
+        {
+            int testCurrentPage = 1;
+            var expectedModel = new TaskPageModel();
+
+            _taskServiceMock
+                .Setup(service => service.GetTasksForPageAsync(It.IsAny<string>(), testCurrentPage))
+                .ReturnsAsync(expectedModel);
+
+            _userController.ControllerContext = CreateController();
+
+            var result = await _userController.MyTasks(testCurrentPage) as ViewResult;
+
+            Assert.NotNull(result);
+            Assert.IsType<ViewResult>(result);
+            Assert.Equal(expectedModel, result.Model);
+            _taskServiceMock.Verify(service => service.GetTasksForPageAsync(userId, testCurrentPage), Times.Once);
+        }
+
+        private ControllerContext CreateController()
+        {
+            var userClaimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userId)
+            }));
+
+            return new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = userClaimsPrincipal }
+            };
         }
     }
 }
